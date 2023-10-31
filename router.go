0bda65fdb79c2f0c31fd0268578e10a7b3d4dc25
Dan Lavine <lavineds@gmail.com>
2023-10-31 10:35:53 -0700

    wip

---
+++ router.go
@@ -0,0 +1,124 @@
+// URL router provides a simple clean way of setting up multiplexed url routes.
+package urlrouter
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type routes map[string]*route
+
+type route struct {
+	namedChildren *route
+	urlChildren   routes
+	handlerFunc   http.HandlerFunc
+}
+
+func (r *route) addUrl(path string, handlerFunc http.HandlerFunc) {
+	splitPaths := strings.SplitN(path, "/", 2)
+
+	fmt.Printf("path: %#v\n", path)
+	fmt.Printf("split path: %#v\n", splitPaths)
+
+	switch len(splitPaths) {
+	case 1:
+		// must be at the end
+
+		// this is a named parameter
+		if strings.HasPrefix(splitPaths[0], ":") {
+			if r.namedChildren == nil {
+				*r.namedChildren = route{handlerFunc: handlerFunc}
+			} else {
+				r.namedChildren.handlerFunc = handlerFunc
+			}
+
+			return
+		}
+
+		// this is a url path
+		if childRoutes, ok := r.urlChildren[splitPaths[0]]; ok {
+			childRoutes.handlerFunc = handlerFunc
+		} else {
+			r.urlChildren[splitPaths[0]] = &route{handlerFunc: handlerFunc}
+		}
+	default: // always will be 2
+		// must be able to recurse
+
+		// this is a named parameter
+		if strings.HasPrefix(splitPaths[0], ":") {
+			if r.namedChildren == nil {
+				*r.namedChildren = route{}
+			}
+
+			r.namedChildren.addUrl(splitPaths[1], handlerFunc)
+			return
+		}
+
+		// this is a url path
+		if childRoutes, ok := r.urlChildren[splitPaths[0]]; ok {
+			childRoutes.addUrl(splitPaths[1], handlerFunc)
+		} else {
+			r.urlChildren[splitPaths[0]] = &route{}
+			r.urlChildren[splitPaths[0]].addUrl(splitPaths[1], handlerFunc)
+		}
+	}
+}
+
+func (route *route) serveHTTP(path string, w http.ResponseWriter, r *http.Request) {
+	splitPaths := strings.SplitN(path, "/", 2)
+
+	switch len(splitPaths) {
+	case 1:
+		// this is a proper url found
+		if urlChild, ok := route.urlChildren[splitPaths[0]]; ok {
+			urlChild.namedChildren.handlerFunc(w, r)
+			return
+		}
+
+		// check to see if it is a named parameter
+		if route.namedChildren != nil {
+			route.namedChildren.handlerFunc(w, r)
+			return
+		}
+
+		// must not be found
+		http.NotFoundHandler().ServeHTTP(w, r)
+	default:
+	}
+}
+
+type Router struct {
+	routes routes
+}
+
+func New() *Router {
+	return &Router{
+		routes: routes{},
+	}
+}
+
+// Add a new url handler to the router. If a route already exists with the same url
+// path, then this will overwrite the previous handler.
+func (router *Router) HandleFunc(method string, path string, handlerFunc http.HandlerFunc) {
+	var foundRoute *route
+
+	if knownRoute, ok := router.routes[method]; ok {
+		foundRoute = knownRoute
+	} else {
+		foundRoute = &route{}
+		router.routes[method] = foundRoute
+	}
+
+	foundRoute.addUrl(path, handlerFunc)
+}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+
+	if route, ok := router.routes[method]; ok {
+		route.serveHTTP(r.URL.Path, w, r)
+	} else {
+		http.NotFoundHandler().ServeHTTP(w, r)
+	}
+}
---
+++ router_test.go
@@ -0,0 +1,39 @@
+package urlrouter
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestRouter_SimplePathPatterns(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	client := &http.Client{}
+
+	foundHandler := func(path string) func(w http.ResponseWriter, r *http.Request) {
+		return func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(path))
+		}
+	}
+
+	t.Run("It can add just a slash", func(t *testing.T) {
+		path := "/"
+
+		router := New()
+		router.HandleFunc("POST", path, foundHandler(path))
+
+		testServer := httptest.NewServer(router)
+		defer testServer.Close()
+
+		request, err := http.NewRequest("POST", "/", nil)
+		g.Expect(err).ToNot(HaveOccurred())
+
+		resp, err := client.Do(request)
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(resp.StatusCode).To(Equal(http.StatusOK))
+	})
+}
